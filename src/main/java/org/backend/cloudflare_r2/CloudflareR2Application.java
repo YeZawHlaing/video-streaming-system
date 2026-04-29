ce5c379520c0ea7ef93e20487f215597b33f0558
yezawhlaing <yezawhlaing0@gmail.com>
2026-04-29 21:07:48 +0630

    initial commit

---
+++ src/main/java/org/backend/cloudflare_r2/CloudflareR2Application.java
@@ -0,0 +1,13 @@
+package org.backend.cloudflare_r2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudflareR2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloudflareR2Application.class, args);
+    }
+
+}
---
+++ src/test/java/org/backend/cloudflare_r2/CloudflareR2ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.backend.cloudflare_r2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudflareR2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
