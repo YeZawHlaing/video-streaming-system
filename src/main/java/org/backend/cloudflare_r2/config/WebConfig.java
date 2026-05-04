b2f4c18d698d66ded0a240c99a029a36d8d0b17c
yezawhlaing <yezawhlaing0@gmail.com>
2026-05-04 07:43:55 +0630

    specific url

--- src/main/java/org/backend/cloudflare_r2/config/WebConfig.java
+++ src/main/java/org/backend/cloudflare_r2/config/WebConfig.java
@@ -10,7 +10,8 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") // Allow all endpoints
-                .allowedOrigins("https://video-streaming-platform.yzaw3684.workers.dev") // Your specific frontend
+                .allowedOrigins("https://video-streaming-platform.yzaw3684.workers.dev",
+                        "https://video-streaming-platform.yzaw3684.workers.dev/") // Your specific frontend
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Allowed HTTP methods
                 .allowedHeaders("*") // Allow all headers
                 .allowCredentials(true);
