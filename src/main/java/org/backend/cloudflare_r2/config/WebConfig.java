2b6cf1f958ad8732e50124372ef7249fb9e0d3bc
yezawhlaing <yezawhlaing0@gmail.com>
2026-05-04 08:46:04 +0630

    cors

--- src/main/java/org/backend/cloudflare_r2/config/WebConfig.java
+++ src/main/java/org/backend/cloudflare_r2/config/WebConfig.java
@@ -1,19 +1,19 @@
-package org.backend.cloudflare_r2.config;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-@Configuration
-public class WebConfig implements WebMvcConfigurer {
-
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**") // Allow all endpoints
-                .allowedOrigins("https://video-streaming-platform.yzaw3684.workers.dev",
-                        "https://video-streaming-platform.yzaw3684.workers.dev/") // Your specific frontend
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Allowed HTTP methods
-                .allowedHeaders("*") // Allow all headers
-                .allowCredentials(true);
-    }
-}
+//package org.backend.cloudflare_r2.config;
+//
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.web.servlet.config.annotation.CorsRegistry;
+//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+//
+//@Configuration
+//public class WebConfig implements WebMvcConfigurer {
+//
+//    @Override
+//    public void addCorsMappings(CorsRegistry registry) {
+//        registry.addMapping("/**") // Allow all endpoints
+//                .allowedOrigins("https://video-streaming-platform.yzaw3684.workers.dev",
+//                        "https://video-streaming-platform.yzaw3684.workers.dev/") // Your specific frontend
+//                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Allowed HTTP methods
+//                .allowedHeaders("*") // Allow all headers
+//                .allowCredentials(true);
+//    }
+//}
--- src/main/java/org/backend/cloudflare_r2/controller/VideoController.java
+++ src/main/java/org/backend/cloudflare_r2/controller/VideoController.java
@@ -19,6 +19,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/videos")
 @RequiredArgsConstructor
+@CrossOrigin
 public class VideoController {
 
     private final R2Service r2Service;
